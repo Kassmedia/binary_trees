cd38a55891aed73f7370190be667a51cc351bc0b
wahab <wabsid01@gmail.com>
2024-03-27 01:52:41 -0700

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,23 +2,23 @@
 /**
 * binary_tree_depth - measures the depth of a node
 * @tree: pointer to node measures the depth
-* Return:  measure the depth
+* Return:  measure the depth of the node
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	unsigned int depth;
-	binary_tree_t *current;
+	binary_tree_t *new;
 
 	if (tree == NULL)
 		return (0);
 
 	depth = 0;
-	
-	current = tree->parent;
-	while (current)
+
+	new = tree->parent;
+	while (new)
 	{
 		depth++;
-		current = current->parent;
+		new = new->parent;
 	}
 	return (depth);
 }
